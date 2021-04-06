c09aa6ab7c4a6ab5e0393caee8969db0251fd6f7
OtabekVaxobov <elot2017@mail.ru>
2021-04-06 11:02:15 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import MDEditor from '@uiw/react-md-editor';
 
-function App() {
+export default function App() {
+  const [value, setValue] = React.useState("**Hello world!!!**");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <MDEditor
+        value={value}
+        onChange={setValue}
+        highlightEnable= {true}
+      />
+      <MDEditor.Markdown source={value} />
     </div>
   );
 }
-
-export default App;
